0363c5f7569417a1dcbabfd62c6fd974b164c496
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-08-23 04:59:57 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,31 +2,18 @@
 """ Minimum Operations
     """
 
-MAXN = 100001
-
-# stores smallest prime factor for every number
-spf = [1] * (MAXN + 1)
-
-# Calculating SPF (Smallest Prime Factor) for every number till MAXN.
-# Time Complexity : O(nloglogn)
-def sieve():
-    """ Calculate all spf """
-    for i in range(2, MAXN + 1):
-        if spf[i] == 1:  # If the number is prime
-            for j in range(i, MAXN + 1, i):
-                if spf[j] == 1:
-                    spf[j] = i
-
-def minOperations(n):
-    """ Minimum Operations needed to get n H characters """  
+def minOperations(n: int) -> int:
+    """ Calculate the minimum operations needed to get n H characters """
     if n <= 1:
         return 0
-    
-    sieve()
-    
-    sum_operations = 0
-    while n > 1:
-        sum_operations += spf[n]
-        n = n // spf[n]
 
-    return sum_operations
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    
+    return operations
